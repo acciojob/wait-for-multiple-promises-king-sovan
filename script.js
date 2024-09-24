0307ca67fa787663af392a5b082107534a07d2eb
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-09-24 12:27:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ const promise1 = createPromise('Promise 1');
 const promise2 = createPromise('Promise 2');
 const promise3 = createPromise('Promise 3');
 
-const startTime = performance.mow();
+const startTime = performance.now();
 
 Promise.all([promise1, promise2, promise3]).then((results) => {
 
 	document.getElementById('loading').remove();
 
-	const totatTime = ((performance.now() - startTime / 1000).toFixed(3));
+	const totalTime = ((performance.now() - startTime / 1000).toFixed(3));
 
 	results.forEach((result) => {
 		const row = document.createElement('tr');
